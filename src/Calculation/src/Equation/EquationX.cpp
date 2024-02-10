33cb29b6fcc6be58996026172f47346319e5a4ca
Савватей Степанов <savva2609@mail.ru>
2024-02-10 18:29:26 +0700

    add: Deposit calculation

--- src/Calculation/src/Equation/EquationX.cpp
+++ src/Calculation/src/Equation/EquationX.cpp
@@ -2,57 +2,56 @@
 
 namespace Equations
 {
-EquationWithX::EquationWithX(std::shared_ptr<std::list<std::shared_ptr<Operations::IOperation>>> operations,
-                             std::shared_ptr<Operations::NumberOperation> x)
-    : Equation(operations)
-{
-    _x = x;
-}
-
-double EquationWithX::Execute(double x)
-{
-    _x->SetNumber(x);
-    Equation &equation = *this;
-    return equation();
-}
-
-std::vector<double> EquationWithX::Execute(double start, double end, size_t numberOfSteps)
-{
-    std::vector<double> result = {};
-    Execute(start, end, numberOfSteps, result);
-
-    return std::move(result);
-}
-
-void EquationWithX::Execute(double start, double end, size_t numberOfSteps, std::vector<double> &result)
-{
-    if (start >= end)
+    EquationWithX::EquationWithX(std::shared_ptr<std::list<std::shared_ptr<Operations::IOperation>>> operations,
+                                 std::shared_ptr<Operations::NumberOperation> x)
+        : Equation(operations), _x(x)
     {
-        return;
     }
 
-    if (result.size() < numberOfSteps)
+    double EquationWithX::Execute(double x)
     {
-        result.resize(numberOfSteps);
+        _x->SetNumber(x);
+        Equation &equation = *this;
+        return equation();
     }
 
-    for (int i = 0; i < numberOfSteps; i++)
+    std::vector<double> EquationWithX::Execute(double start, double end, size_t numberOfSteps)
     {
-        (result)[i] = Execute(i * (end - start) / (numberOfSteps) + start);
+        std::vector<double> result = {};
+        Execute(start, end, numberOfSteps, result);
+
+        return std::move(result);
     }
-}
 
-double EquationWithX::operator()(double x)
-{
-    return Execute(x);
-}
+    void EquationWithX::Execute(double start, double end, size_t numberOfSteps, std::vector<double> &result)
+    {
+        if (start >= end)
+        {
+            return;
+        }
 
-std::vector<double> EquationWithX::operator()(double start, double end, size_t numberOfSteps)
-{
-    return Execute(start, end, numberOfSteps);
-}
-void EquationWithX::operator()(double start, double end, size_t numberOfSteps, std::vector<double> &result)
-{
-    Execute(start, end, numberOfSteps, result);
-}
+        if (result.size() < numberOfSteps)
+        {
+            result.resize(numberOfSteps);
+        }
+
+        for (int i = 0; i < numberOfSteps; i++)
+        {
+            (result)[i] = Execute(i * (end - start) / (numberOfSteps) + start);
+        }
+    }
+
+    double EquationWithX::operator()(double x)
+    {
+        return Execute(x);
+    }
+
+    std::vector<double> EquationWithX::operator()(double start, double end, size_t numberOfSteps)
+    {
+        return Execute(start, end, numberOfSteps);
+    }
+    void EquationWithX::operator()(double start, double end, size_t numberOfSteps, std::vector<double> &result)
+    {
+        Execute(start, end, numberOfSteps, result);
+    }
 } // namespace Equations
---
+++ src/Deposit/src/Deposit.cpp
@@ -0,0 +1,53 @@
+#include <Deposit/Deposit>
+
+namespace Deposit
+{
+
+    Deposit::Deposit(const DepositInfo &info) : _currentState(info)
+    {
+    }
+
+    Deposit::Deposit(const DepositInfo &info, std::queue<Payment> &&inputPayments, std::queue<Payment> &&outputPayments) : _currentState(info),
+                                                                                                                           _inputPayments(std::move(inputPayments)),
+                                                                                                                           _outputPayments(std::move(outputPayments))
+    {
+    }
+
+    EndStatistic Deposit::ComputeEndStatistic()
+    {
+
+        EndStatistic result = {.Amount = _currentState.InitialAmount, .AccruedInterest = 0, .TotalTax = 0};
+        Day currentDay = 1;
+        while (currentDay <= _currentState.Term)
+        {
+
+            if (!_inputPayments.empty() && _inputPayments.front().DayOfPayment == currentDay)
+            {
+                result.Amount += _inputPayments.front().Amount;
+                _inputPayments.pop();
+            }
+
+            if (!_outputPayments.empty() && _outputPayments.front().DayOfPayment == currentDay)
+            {
+                result.Amount -= _outputPayments.front().Amount;
+                _outputPayments.pop();
+            }
+
+            if (currentDay % _currentState.PeriodOfPayment == 0)
+            {
+                Money percent = result.Amount * _currentState.InterestRate / 365.0 * _currentState.PeriodOfPayment;
+                if (_currentState.Capitalization)
+                {
+                    percent += result.AccruedInterest * _currentState.InterestRate / 365.0 * _currentState.PeriodOfPayment;
+                }
+                result.TotalTax += percent * _currentState.TaxRate;
+                result.AccruedInterest += percent * (1.0 - _currentState.TaxRate);
+            }
+            currentDay++;
+        }
+
+        result.Amount += result.AccruedInterest;
+
+        return result;
+    }
+}
